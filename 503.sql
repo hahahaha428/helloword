d9e08446f42d8064ecf48951b887a6f8e0f7c9cb
jd_4c5335fa80a50 <lizhe_0428@163.com>
2015-06-18 14:04:36 +0800

    ssfdsfs

---
+++ 503.sql
@@ -0,0 +1,8 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.6.10-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `c_direct_shop` (`id`, `province`, `city`, `area`, `name`, `sort`, `create_date`, `modify_date`, `parent_id`, `direct_shop_id`, `type`, `employee_id`, `build_date`, `address`, `phone`, `fax`, `inner_coefficient`, `allocate_coefficient`, `real_coefficient`, `store_level`, `store_sn`, `parent_storage_id`, `bank`, `bank_account`, `remark`, `department_printname`, `store_type`, `is_show`, `manager_name`, `manager_phone`) values('503',NULL,NULL,NULL,'重庆江北仓库',NULL,'2015-05-04 16:24:56','2015-05-04 16:24:56','0564','0565','20',NULL,NULL,'','','',NULL,NULL,NULL,'0','','0535','','',NULL,'','1','1',NULL,NULL);
